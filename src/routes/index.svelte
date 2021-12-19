2b32c3307d4f01eb95666f398981f3e62d7afe92
battlegamez <ultralanpasalan0@gmail.com>
2021-12-19 04:02:19 +0000

    eee

---
+++ src/routes/index.svelte
@@ -0,0 +1,20 @@
+<script>
+let age = ""
+let name = ""
+
+async function getName() {
+    fetch('https://api.agify.io/?name=' + name)
+    .then((response) => response.json())
+    .then((data) => {
+        age = data.age
+    })
+}
+</script>
+
+<h1>Weather App</h1>
+<p>Age: {age}</p>
+<form on:submit|preventDefault={getName}>
+    <label for="name">Name:</label>
+    <input id="name" bind:value={name} type="text" />
+    <button type="submit">Check</button>
+</form>
